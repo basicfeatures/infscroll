faad6b2fc1933ecf9c679ec8d617f541fd4ea2f6
Your Name <you@example.com>
2022-08-28 09:10:50 +0000

    add index to FrontpageController

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,2 +1,10 @@
 class FrontpageController < ApplicationController
+  def index
+    @pagy, @posts = pagy_countless(Post.order(created_at: :desc), items: 5)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
+  end
 end
