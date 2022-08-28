9a93db416d95afca8e37ab31396d302bbb006429
Your Name <you@example.com>
2022-08-28 13:55:22 +0000

    attempt to replicate bearer's example: https://www.bearer.com/blog/infinite-scrolling-pagination-hotwire

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      post :index
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
